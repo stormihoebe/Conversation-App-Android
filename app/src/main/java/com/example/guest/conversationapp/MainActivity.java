e0d78ff426f73368148997cbde78af6b857578f2
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-24 13:42:52 -0700

    add dialog to ask question

--- app/src/main/java/com/example/guest/conversationapp/MainActivity.java
+++ app/src/main/java/com/example/guest/conversationapp/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.guest.conversationapp;
 
 import android.app.Dialog;
 import android.app.FragmentManager;
+import android.support.annotation.BinderThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.moodButton)
+    Button moodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        FragmentManager fm = getFragmentManager();
-        MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
-        moodDialogFragment.show(fm, "Sample Fragment");
+        moodButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FragmentManager fm = getFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/example/guest/conversationapp/MoodDialogFragment.java
+++ app/src/main/java/com/example/guest/conversationapp/MoodDialogFragment.java
@@ -1,24 +1,73 @@
 package com.example.guest.conversationapp;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Created by Guest on 5/24/17.
  */
 
 public class MoodDialogFragment extends DialogFragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        getDialog().setTitle("Simple Dialog to test");
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup);
+        int selectedId = surveyRadioGroup.getCheckedRadioButtonId();
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing output", selectedRadioButton.getText().toString());
+                dismiss();
+            }
+        });
         return rootView;
+        }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Dialog via Builder");
+        builder.setMessage("Would you like to take a survey?");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        return builder.create();
     }
+
+
 }
